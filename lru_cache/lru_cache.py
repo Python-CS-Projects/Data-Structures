e0870cbf0828e6c59e7627e10e0b73ea82d2915f
fritzgt <tuanis@live.com>
2020-02-18 06:05:06 +0200

    working on lru

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,8 @@
+from doubly_linked_list import DoublyLinkedList
+import sys
+sys.path.append('./doubly_linked_list')
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -6,8 +11,12 @@ class LRUCache:
     order, as well as a storage dict that provides fast access
     to every node stored in the cache.
     """
+
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        self.size = 0
+        self.order = DoublyLinkedList()
+        self.storage = dict()
 
     """
     Retrieves the value associated with the given key. Also
@@ -16,8 +25,17 @@ class LRUCache:
     Returns the value associated with the key or None if the
     key-value pair doesn't exist in the cache.
     """
+    # UPDATE THE POSITION OF THE RECENTLY GET OR RECENTLY USE KEY
+    # SO THE RECENTLY USE GOES TO ??
+
     def get(self, key):
-        pass
+        if key in self.storage:
+            node = self.storage[key]
+        # move value to the tail as recently used
+            self.order.move_to_front(node)
+            return node
+        else:
+            return None
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -29,5 +47,21 @@ class LRUCache:
     want to overwrite the old value associated with the key with
     the newly-specified value.
     """
+    # IF THE CACHE IS FULL THE NEW VALUE TAKE PLACE OF THE OLDEST VALUE
+    # THE OLDEST GOES TO ?? WHICH CAN POTENTIALLY BE OVERWRITTEN IF NO SPACE
+
     def set(self, key, value):
-        pass
+        if key in self.storage.keys():
+            self.order.move_to_front(value)
+
+        # if not full add to tail and substract from limit 10
+        elif self.size < self.limit:
+
+            self.order.add_to_head(value)
+            self.storage[key] = self.order.head
+            self.size += 1
+         # if the chache is full remove from head and add value to tail
+        else:
+            self.storage[key] = value
+            self.order.remove_from_tail()
+            self.order.add_to_head(value)
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -10,19 +10,13 @@ class Stack:
         self.storage = DoublyLinkedList()
 
     def push(self, value):
-        if self.size > 0:
-            self.storage.add_to_tail(value)
-            self.size += 1
-        else:
-            self.storage.add_to_tail(value)
-            self.size += 1
+        self.storage.add_to_tail(value)
+        self.size += 1
 
     def pop(self):
         if self.size > 0:
             self.size -= 1
             return self.storage.remove_from_tail()
-        else:
-            return None
 
     def len(self):
         return self.size
